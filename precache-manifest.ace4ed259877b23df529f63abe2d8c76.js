bdd4f84bd4a6efa8ec3dfd89bb5975f3cd75cfbb
yerevin <skulskimat@gmail.com>
2019-01-14 23:51:18 +0100

    Updates

---
+++ precache-manifest.ace4ed259877b23df529f63abe2d8c76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aba6e21b5ed9a021840",
+    "url": "/self-improver/static/js/runtime~main.1aba6e21.js"
+  },
+  {
+    "revision": "f714b457da792b617de1",
+    "url": "/self-improver/static/js/main.f714b457.chunk.js"
+  },
+  {
+    "revision": "ec10292e1ae5dabe9c85",
+    "url": "/self-improver/static/js/1.ec10292e.chunk.js"
+  },
+  {
+    "revision": "f714b457da792b617de1",
+    "url": "/self-improver/static/css/main.057202b0.chunk.css"
+  },
+  {
+    "revision": "416a8b3f3d1d1a4e20de1e6737765539",
+    "url": "/self-improver/index.html"
+  }
+];
